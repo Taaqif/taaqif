fa17fe7f4dfbf886402a76659c1a8053f0d4fd37
Tye Peck <tye@novatasolutions.com.au>
2024-11-14 21:01:24 +0800

    feat: add hide system cursor and clickables

--- src/components/StickyCursor.tsx
+++ src/components/StickyCursor.tsx
@@ -1,11 +1,24 @@
 "use client";
 import { useSpring } from "@react-spring/web";
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
 import { a } from "@react-spring/web";
-const clickables = ["A"];
-export default function StickyCursor() {
+import { useInjectStyle } from "use-inject-style";
+
+type StickyCursorProps = {
+  clickables?: string[];
+  showSystemCursor?: boolean;
+};
+export default function StickyCursor({
+  clickables = ["A"],
+  showSystemCursor = false,
+}: StickyCursorProps) {
   const cursorSize = 10;
   const cursorOuterSize = cursorSize * 5;
+  const { inject, remove } = useInjectStyle("no-cursur-styles");
+  const noCursorStyles = useMemo(
+    () => `${clickables?.join(",")} {cursor: none}`,
+    [clickables],
+  );
   const [isVisible, setIsVisible] = useState(false);
   const [isHovering, setIsHovering] = useState(false);
   const [isTouchdevice, setIsTouchdevice] = useState<boolean>();
@@ -13,8 +26,8 @@ export default function StickyCursor() {
   const startingX = typeof window !== "undefined" ? window.innerWidth / 2 : 0;
   const startingY = typeof window !== "undefined" ? window.innerHeight / 2 : 0;
   const [cursorPosition, setCursorPosition] = useSpring(() => ({
-    left: startingX,
-    top: startingY,
+    left: startingX - cursorSize / 2,
+    top: startingY - cursorSize / 2,
     config: {
       tension: 300,
       friction: 20,
@@ -22,11 +35,11 @@ export default function StickyCursor() {
     },
   }));
   const [cursorOuterPosition, setCursorOuterPosition] = useSpring(() => ({
-    left: startingX,
-    top: startingY,
+    left: startingX - cursorOuterSize / 2,
+    top: startingY - cursorOuterSize / 2,
     config: {
       tension: 300,
-      friction: 40,
+      friction: 50,
       mass: 0.5,
     },
   }));
@@ -59,38 +72,44 @@ export default function StickyCursor() {
         left: clientX - cursorOuterSize / 2,
         top: clientY - cursorOuterSize / 2,
       });
+      const elementsMouseIsOver = document.elementsFromPoint(clientX, clientY);
+      const isClickable = elementsMouseIsOver.some((elem) =>
+        clickables.includes(elem.tagName),
+      );
+      setIsHovering(isClickable);
+      setIsVisible(true);
     },
-    [setCursorOuterPosition, setCursorPosition, cursorOuterSize],
+    [setCursorOuterPosition, setCursorPosition, cursorOuterSize, clickables],
   );
 
-  const onMouseEnterViewport = useCallback((e: MouseEvent) => {
-    const { clientX: x, clientY: y } = e;
-    const elementsMouseIsOver = document.elementsFromPoint(x, y);
-    const isClickable = elementsMouseIsOver.some((elem) =>
-      clickables.includes(elem.tagName),
-    );
-    setIsHovering(isClickable);
-    setIsVisible(true);
-  }, []);
   const onMouseExitViewport = useCallback(() => setIsVisible(false), []);
 
   useEffect(() => {
     window.addEventListener("mousemove", manageMouseMove);
-    window.addEventListener("mouseover", onMouseEnterViewport);
+    window.addEventListener("mouseover", manageMouseMove);
     window.addEventListener("mouseout", onMouseExitViewport);
 
     return () => {
+      window.removeEventListener("mouseover", manageMouseMove);
       window.removeEventListener("mousemove", manageMouseMove);
-      window.removeEventListener("mouseover", onMouseEnterViewport);
       window.removeEventListener("mouseout", onMouseExitViewport);
     };
-  }, [manageMouseMove, onMouseEnterViewport, onMouseExitViewport]);
+  }, [manageMouseMove, onMouseExitViewport]);
 
   useEffect(() => {
     if (typeof window !== "undefined") {
       setIsTouchdevice(window.matchMedia("(hover: none)").matches);
     }
   }, []);
+
+  useEffect(() => {
+    if (typeof window === "object" && !showSystemCursor) {
+      document.body.style.cursor = "none";
+      inject(noCursorStyles);
+    } else {
+      remove();
+    }
+  }, [showSystemCursor, inject, noCursorStyles, remove]);
 
   if (isTouchdevice) {
     return <></>;
